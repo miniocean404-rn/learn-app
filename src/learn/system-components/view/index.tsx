88f53c4b9c0b19fb98170cbe25ff3aede745f2d2
MiniOcean404 Davinci <haiyang.dai@davincimotor.com>
2023-04-25 18:10:13 +0800

    feat: view 组件

--- src/learn/system-components/view/index.tsx
+++ src/learn/system-components/view/index.tsx
@@ -1,13 +1,21 @@
 import { LayoutChangeEvent, SafeAreaView, StatusBar, View } from 'react-native'
 import styles from './styles'
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 const SystemView = () => {
   const [height, setHeight] = useState(100)
+  const viewRef = useRef<View>(null)
 
   useEffect(() => {
     setTimeout(() => {
       setHeight(200)
+
+      // 性能瓶颈下使用这个进行设置属性，并不会重新刷新整个视图，而是直接设置原生属性
+      viewRef.current?.setNativeProps({
+        style: {
+          backgroundColor: 'yellow',
+        },
+      })
     }, 1000)
   }, [])
 
@@ -21,7 +29,7 @@ const SystemView = () => {
       <StatusBar />
 
       <View style={styles.root}>
-        <View style={[styles.subView1, { height }]} onLayout={onLayout}></View>
+        <View style={[styles.subView1, { height }]} onLayout={onLayout} ref={viewRef}></View>
         <View style={styles.subView2}></View>
         <View style={styles.subView3}></View>
       </View>
