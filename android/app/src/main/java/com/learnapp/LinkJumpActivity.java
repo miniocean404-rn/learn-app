ee799f2e7476954748b5b59a34f97d4ef8b49005
MiniOcean404 Davinci <haiyang.dai@davincimotor.com>
2023-04-27 18:17:53 +0800

    feat: api 跳转

---
+++ android/app/src/main/java/com/learnapp/LinkJumpActivity.java
@@ -0,0 +1,19 @@
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.learnapp.R;
+
+public class LinkJumpActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+
+        setContentView(R.layout.activity_link_jump);
+
+        TextView tv = findViewById(R.id.tv_link_jump);
+    }
+}
